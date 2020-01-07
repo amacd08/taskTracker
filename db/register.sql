1ff7fdb6c8a03c865c3d8dbdd0572344d5325310
Andrew Mcdonald <andmcd@fb.com>
2020-01-07 15:30:25 -0700

    troubleshoot register

--- db/register.sql
+++ db/register.sql
@@ -1,7 +1,6 @@
 insert into users (firstname, lastname, email)
 values (${firstName}, ${lastName}, ${email});
 insert into user_login (username, password))
-values (${username}, ${password})
-returning username, user_id;
+values (${username}, ${password});
 
 
