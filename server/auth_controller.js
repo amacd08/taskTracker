583e973b8398e73e1f71c062812ad6501f0ff19d
Andrew Mcdonald <andmcd@fb.com>
2020-01-07 15:16:57 -0700

    troubleshoot register

--- server/auth_controller.js
+++ server/auth_controller.js
@@ -7,7 +7,7 @@ register: async (req, res) => {
     const db = req.app.get('db')
     const userFound = await db.checkUserEmail({email})
     if (userFound[0]) return res.status(409).send('Email already exists')
-    const salt = bcrypt.genSaltSync(10)
+        const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(password, salt)
         const createdUser = await db.register({
             firstName,
@@ -15,9 +15,7 @@ register: async (req, res) => {
             username,
             email,
             password: hash
-        })
-        console.log(createdUser)
-        createdUser.catch(err => console.log(err))
+        }).catch(err => console.log(err))
         session.user = {id: createdUser[0].user_id, username: createdUser[0].username}
         res.status(200).send(session.user)
     }
